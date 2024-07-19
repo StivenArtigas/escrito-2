26608289f63b9d3b45246599e2657968d71e8d4e
StivenArtigas <stiven.artigas@estudiante.ceibal.edu.uy>
2024-07-19 12:27:21 -0300

    cargada de base de datos

--- persistencia/Class1.cs
+++ persistencia/Class1.cs
@@ -8,5 +8,43 @@ namespace persistencia
 {
     public class base_de_datos
     {
+        public int Id;
+        public string Nombre;
+        public string Apellido;
+
+        public void Guardar()
+        {
+            string sql = $"INSERT INTO personas (nombre, apellido) VALUES('{this.Nombre}',{this.Apellido})";
+            this.Comando.CommandText = sql;
+            this.Comando.ExecuteNonQuery();
+
+        }
+
+        public void Borrar()
+        {
+            string sql = $"UPDATE personas SET eliminado = true WHERE id ='{this.Id}'";
+            this.Comando.CommandText = sql;
+            this.Comando.ExecuteNonQuery();
+        }
+
+        public List<base_de_datos> ObtenerTodos()
+        {
+            List<base_de_datos> bd = new List<base_de_datos>();
+
+            string sql = $"SELECT * FROM personas WHERE eliminado = false";
+            this.Comando.CommandText = sql;
+            this.Lector = this.Comando.ExecuteReader();
+
+            while (this.Lector.Read())
+            {
+                base_de_datos bd = new base_de_datos();
+                bd.Id = Int32.Parse(this.Lector["Id"].ToString());
+                bd.Nombre = this.Lector["Nombre"].ToString();
+                bd.Precio = this.Lector["Apellido"].ToString());
+                bd.Add(pizza);
+            }
+            return bd;
+
+        }
     }
 }
---
+++ persistencia/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace persistencia
+{
+    public abstract class conexion_base_de_datos
+    {
+        public string IP;
+        public string NombreBase;
+        public string NombreDeUsuario;
+        public string Password;
+
+        public MySqlConnection Conexion;
+        public MySqlCommand Comando;
+        public MySqlDataReader Lector;
+
+        public Modelo()
+        {
+            this.IP = "170.0.0.1";
+            this.NombreBase = "escrito2";
+            this.Password = "Ss11331313$";
+            this.NombreDeUsuario = "root";
+
+            this.Conexion = new MySqlConnection(
+                $"server={this.IP};" +
+                $"user={this.NombreDeUsuario};" +
+                $"password={this.Password};" +
+                $"database={this.NombreBase};"
+            );
+
+            this.Conexion.Open();
+
+            this.Comando = new MySqlCommand();
+            this.Comando.Connection = this.Conexion;
+
+        }
+
+    }
+}
